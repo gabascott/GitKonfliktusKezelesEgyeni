452ff383a942a6869108fb7b7233476456ac3b60
gabascott <78851455+gabascott@users.noreply.github.com>
2022-03-11 14:44:05 +0100

    2. név inicializálása

--- src/konflikzuskezelesgyak/KonflikzuskezelesGyak.java
+++ src/konflikzuskezelesgyak/KonflikzuskezelesGyak.java
@@ -5,7 +5,7 @@ public class KonflikzuskezelesGyak {
     public static void main(String[] args) {
         
         String nev1;
-        String nev2;
+        String nev2 = "Ichigo";
     }
     
 }
